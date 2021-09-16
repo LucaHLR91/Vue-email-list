f6a57e037edf1adf06157965714a49125ab5b8f1
LucaHLR <mastrantonio.luca.91@icloud.com>
2021-09-16 15:36:47 +0200

    10 emails generation

--- js/main.js
+++ js/main.js
@@ -8,5 +8,17 @@ Descrizione:
 Vue.config.devtools = true;
 const app = new Vue({
     el: '#root',
-    data:{},
+    data:{
+        emails: [],
+    },
+    mounted() {
+       for(let i = 0; i < 10; i++) {
+           axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(function(response) {
+                let email = response.data.response;
+                app.emails.push(email);
+            })
+       }
+       console.log(this.emails)
+    }
 })
